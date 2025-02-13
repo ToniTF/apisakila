0f6fc651a4830308c42b3d89e8a6dad3d16dae1e
Tonitf <subcero00@hotmail.com>
2025-02-13 17:35:07 +0100

    config samples

--- config_sample.php
+++ config_sample.php
@@ -3,4 +3,5 @@ define("HOST", "dirección del servidor");
 define("DB_USER","usuario bbdd");
 define("DB_PASS","password bbdd");
 define("DB_DATABASE","bbdd");
+define("JWT_SECRET_KEY","tu_contraseña_supersecreta"); // Secret key for JWT
 ?>
